bb35c4f9dd473df9794a5f3bac132c915ef4324c
morgan <morgan.fula@me.com>
2023-01-04 16:08:02 +0100

    adding pages 📑

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<h2>Home</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. At similique
+			libero obcaecati non eum velit voluptates, animi numquam totam
+			consectetur.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. At similique
+			libero obcaecati non eum velit voluptates, animi numquam totam
+			consectetur.
+		</p>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+	h2 {
+		margin-bottom: 20px;
+		font-size: 32px;
+	}
+
+	p {
+		margin: 20px 0;
+	}
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<p>Products</p>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+	h2 {
+		margin-bottom: 20px;
+		font-size: 32px;
+	}
+
+	p {
+		margin: 20px 0;
+	}
+</style>

1ae129c26d31a7dd3a654318cf98089dd28365c6
morgan <morgan.fula@me.com>
2023-01-04 16:14:00 +0100

    route parameters 🛣️

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<p>product details for {{ id }}</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Est perspiciatis
+			error minima aliquid rerum eveniet ab illo placeat, inventore assumenda.
+			Aut, modi qui incidunt excepturi doloribus corporis sed cum laborum.
+		</p>
+	</div>
+</template>
+
+<script setup>
+	const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
